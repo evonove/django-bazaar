a1fb5763baab76a0664920d4c0ec71353550606a
Federico Frenguelli <synasius@gmail.com>
2013-09-23 17:24:24 +0200

    add list and create view for products

--- bazaar/goods/views.py
+++ bazaar/goods/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.db import models
+from django.views import generic
+
+from .models import Product
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = "bazaar/goods/product_list.html"
+
+
+class ProductCreateView(generic.CreateView):
+    model = Product
+    template_name = "bazaar/goods/product_form.html"
