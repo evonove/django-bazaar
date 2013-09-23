e3d998134de50dc57330329fc30aa17ef9733102
Federico Frenguelli <synasius@gmail.com>
2013-09-23 17:28:26 +0200

    add crispy forms to dependency. removed model utils

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Pillow',
-        'django-model-utils',
+        'django-cripsy-forms>=1.4.0',
     ],
     license="BSD",
     zip_safe=False,
