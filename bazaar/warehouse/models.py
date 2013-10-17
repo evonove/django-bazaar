a39d0319d7d6eeeed722cd85490b05f862e63f9c
Federico Frenguelli <synasius@gmail.com>
2013-10-17 11:04:37 +0200

    Fixed validation error message

--- bazaar/warehouse/models.py
+++ bazaar/warehouse/models.py
@@ -28,7 +28,7 @@ class RealGood(models.Model):
     def clean(self):
         if self.price.currency.code != bazaar_settings.DEFAULT_CURRENCY:
             raise ValidationError("Price field must contain prices in the system currency."
-                                  "Store the price with in the original currency in original_price field")
+                                  "Store the original price in original_price field")
 
     def __str__(self):
         return _("Real good %s - %s in %s") % (self.uid, self.good, self.warehouse)
