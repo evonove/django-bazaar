3017a2b93c455e54f3fa5a36829052da02f79453
Federico Frenguelli <synasius@gmail.com>
2013-10-17 16:26:57 +0200

    Uniform urls namespace

--- bazaar/urls.py
+++ bazaar/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import patterns, url, include
 from .views import HomeView
 
 
-urlpatterns = patterns(
+bazaar_patterns = patterns(
     '',
     url(r'^$', HomeView.as_view(), name="home"),
-    url(r'', include("bazaar.goods.urls", namespace="bazaar")),
+    url(r'', include("bazaar.goods.urls")),
+)
+
+# uniform namespace for all urls
+urlpatterns = patterns(
+    '',
+    url(r'', include(bazaar_patterns, namespace="bazaar", app_name="bazaar"))
 )
