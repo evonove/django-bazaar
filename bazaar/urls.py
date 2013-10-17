185152155fd69a648f83313a6b5cc589fb6ad934
Federico Frenguelli <synasius@gmail.com>
2013-10-17 16:19:02 +0200

    Add home view for bazaar

--- bazaar/urls.py
+++ bazaar/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url, include
 
+from .views import HomeView
+
 
 urlpatterns = patterns(
     '',
+    url(r'^$', HomeView.as_view(), name="home"),
     url(r'', include("bazaar.goods.urls", namespace="bazaar")),
 )
