900d872d4d1f8a593f25ac982e48ac86660955fd
Federico Frenguelli <synasius@gmail.com>
2013-10-30 15:10:34 +0100

    Store name unique

--- bazaar/listings/models.py
+++ bazaar/listings/models.py
@@ -24,7 +24,7 @@ class Listing(models.Model):
 
 @python_2_unicode_compatible
 class Store(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     url = models.URLField(blank=True)
 
     def __str__(self):
