99dc81f6922ec87d0930f91727f3488a3c8e32ba
Federico Frenguelli <synasius@gmail.com>
2013-11-25 15:58:10 +0100

    small improvements to stock admin

--- bazaar/warehouse/admin.py
+++ bazaar/warehouse/admin.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 from .models import Stock, Movement
 
@@ -10,6 +12,8 @@ class StockAdmin(admin.ModelAdmin):
     inlines = [
         MovementInline,
     ]
+    list_display = ('price', 'product', 'quantity')
+    raw_id_fields = ('product',)
 
 
 admin.site.register(Stock, StockAdmin)
