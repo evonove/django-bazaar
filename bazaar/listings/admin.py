2cf36b1545faeba6ede35419646926948703f8f9
Federico Frenguelli <synasius@gmail.com>
2013-12-05 14:49:31 +0100

    Added listing and publishing to admin. Fixes item:19

--- bazaar/listings/admin.py
+++ bazaar/listings/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Store
+from .models import Listing, Publishing, Store
+
+
+class ListingAdmin(admin.ModelAdmin):
+    model = Listing
+    raw_id_fields = ('products',)
 
 
 admin.site.register(Store)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Publishing)
