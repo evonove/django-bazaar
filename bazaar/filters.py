f8b72c4f16a60ebaf63a4eac66ea5a0cc058ed26
Federico Frenguelli <synasius@gmail.com>
2013-12-16 13:44:48 +0100

    Add a base filterset class that add empty entry on choice fields and submit on change

---
+++ bazaar/filters.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from django.utils import six
+from django.utils.translation import ugettext as _
+
+import django_filters
+
+
+class BaseFilterSet(django_filters.FilterSet):
+    def __init__(self, *args, **kwargs):
+        super(BaseFilterSet, self).__init__(*args, **kwargs)
+
+        # this is a workaround for issue https://github.com/alex/django-filter/issues/45
+        for name, _filter in six.iteritems(self.filters):
+            if isinstance(_filter, django_filters.ChoiceFilter):
+                # Add "---------" entry to choice fields.
+                _filter.extra['choices'] = tuple([("", _("---------")), ] +
+                                                 list(_filter.extra['choices']))
+
+        for name, _field in six.iteritems(self.form.fields):
+            # We set the 'onchange' to submit the form automatically
+            _field.widget.attrs['onchange'] = "this.form.submit()"
