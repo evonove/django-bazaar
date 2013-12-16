743b678a2113f7f93d4c0cc27aa73745700a460a
Federico Frenguelli <synasius@gmail.com>
2013-12-16 14:48:24 +0100

    Add filter for low stock listings

--- bazaar/listings/filters.py
+++ bazaar/listings/filters.py
@@ -1,13 +1,27 @@
 from __future__ import unicode_literals
 
+from django import forms
+
 import django_filters
 
+from ..filters import BaseFilterSet
 from .models import Listing
 
 
-class ListingFilter(django_filters.FilterSet):
+class LowStockListingFilter(django_filters.Filter):
+    field_class = forms.BooleanField
+
+    def filter(self, qs, value):
+        if value:
+            qs = qs.filter(pk__in=Listing.objects.low_stock_ids())
+
+        return qs
+
+
+class ListingFilter(BaseFilterSet):
     title = django_filters.CharFilter(lookup_type="icontains")
+    low_stock = LowStockListingFilter()
 
     class Meta:
         model = Listing
-        fields = ['title', "publishings__store", "publishings__available_units"]
+        fields = ["title", "publishings__store", "publishings__available_units"]
