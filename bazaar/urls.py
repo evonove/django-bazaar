e5b24f9b3a8b46e69a8c6822d51f667011e2580b
Federico Frenguelli <synasius@gmail.com>
2013-12-17 18:34:18 +0100

    Add message management page

--- bazaar/urls.py
+++ bazaar/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url, include
 
-from .views import HomeView
+from .views import HomeView, MessagesView
 
 
 bazaar_patterns = patterns(
@@ -8,6 +8,10 @@ bazaar_patterns = patterns(
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^accounts/login/$', 'bazaar.views.login', name="login"),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name="logout"),
+
+    # messages
+    url(r'^messages/$', MessagesView.as_view(), name="messages"),
+    url(r'^api/messages/', include("stored_messages.urls")),
 
     url(r'', include("bazaar.goods.urls")),
     url(r'', include("bazaar.listings.urls")),
--- bazaar/views.py
+++ bazaar/views.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.contrib.auth.views import login as auth_login
 from django.http.response import HttpResponseRedirect
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
+
+from stored_messages.models import Inbox
 
 
 def login(request, *args, **kwargs):
@@ -15,3 +18,13 @@ def login(request, *args, **kwargs):
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+class MessagesView(ListView):
+    model = Inbox
+    paginate_by = 10
+    template_name = "bazaar/inbox_list.html"
+
+    def get_queryset(self):
+        qs = super(MessagesView, self).get_queryset()
+        return qs.select_related("message").order_by("-message__date")
