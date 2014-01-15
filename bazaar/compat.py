8d7b472cf29d01991f8fc5e748bcc2f290c6160a
Federico Frenguelli <synasius@gmail.com>
2014-01-15 15:33:26 +0100

    Fixed django version check in compat module

--- bazaar/compat.py
+++ bazaar/compat.py
@@ -1,6 +1,6 @@
 import django
 
-if django.get_version() < (1, 6):
+if django.VERSION < (1, 6):
     from django.db.transaction import commit_on_success as atomic
 else:
     from django.db.transaction import atomic
