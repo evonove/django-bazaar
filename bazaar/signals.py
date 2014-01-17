d98d52dc65b0b65ab54a7077ac449c1c489952e5
Federico Frenguelli <synasius@gmail.com>
2014-01-17 18:40:13 +0100

    Changed providing_args for product price changed signal

--- bazaar/signals.py
+++ bazaar/signals.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 import django.dispatch
 
 
-product_price_changed = django.dispatch.Signal(providing_args=["product"])
+product_price_changed = django.dispatch.Signal(providing_args=["price"])
 product_stock_changed = django.dispatch.Signal(providing_args=["quantity"])
