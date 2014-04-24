c721bc82b2238a08d37125793eb59fe44747f954
Federico Frenguelli <synasius@gmail.com>
2014-04-24 11:24:29 +0200

    Add tests for product.cost property

--- tests/goods/test_models.py
+++ tests/goods/test_models.py
@@ -3,24 +3,31 @@
 
 from __future__ import unicode_literals
 
+from django.test import TestCase
+
 from bazaar.goods.models import Product, PriceList, ProductPrice
 
+from moneyed import Money
+
 from ..base import BaseTestCase
+from ..factories import ProductFactory, StockFactory
 
 
-class TestProduct(BaseTestCase):
+class TestProduct(TestCase):
     def setUp(self):
-        self.product = Product.objects.create(name="a product")
+        self.product = ProductFactory()
 
     def tearDown(self):
         self.product.delete()
 
     def test_model(self):
-        self.assertEqual(str(self.product), "a product")
+        self.assertEqual("%s" % self.product, "a product")
 
     def test_product_cost_property(self):
-        with self.assertRaises(NotImplementedError):
-            self.product.cost
+        StockFactory(product=self.product, unit_price=10, quantity=10)
+        StockFactory(product=self.product, unit_price=5, quantity=30)
+
+        self.assertEqual(self.product.cost, Money(6.25, "EUR"))
 
 
 class TestPriceList(BaseTestCase):
