272bd88b61ea52b2fd026738951c535809f8ec68
Gioele Meoni <gioelemeoni@gmail.com>
2014-04-28 13:16:13 +0200

    Added high lever signals

--- bazaar/warehouse/signals.py
+++ bazaar/warehouse/signals.py
@@ -5,3 +5,9 @@ import django.dispatch
 
 incoming_movement = django.dispatch.Signal(providing_args=["movement"])
 outgoing_movement = django.dispatch.Signal(providing_args=["movement"])
+
+lost_and_found_changed = django.dispatch.Signal(providing_args=["product"])
+supplier_changed = django.dispatch.Signal(providing_args=["product"])
+storage_changed = django.dispatch.Signal(providing_args=["product"])
+output_changed = django.dispatch.Signal(providing_args=["product"])
+customer_changed = django.dispatch.Signal(providing_args=["product"])
--- bazaar/warehouse/stocks.py
+++ bazaar/warehouse/stocks.py
@@ -10,8 +10,8 @@ import logging
 
 from django.dispatch import receiver
 
-from .models import Stock
-from .signals import incoming_movement, outgoing_movement
+from .models import Stock, Location
+from .signals import incoming_movement, outgoing_movement, lost_and_found_changed, supplier_changed, storage_changed, output_changed, customer_changed
 
 
 logger = logging.getLogger(__name__)
@@ -32,6 +32,7 @@ def update_stock_on_incoming(sender, movement, **kwargs):
     stock.quantity = quantity
     stock.unit_price = avg_unit_price
     stock.save()
+    _changed_location_signal_sender(stock)
 
 
 @receiver(outgoing_movement)
@@ -40,3 +41,17 @@ def update_stock_on_outgoing(sender, movement, **kwargs):
 
     stock.quantity = stock.quantity - movement.quantity
     stock.save()
+    _changed_location_signal_sender(stock)
+
+
+def _changed_location_signal_sender(stock):
+    if stock.location.type == Location.LOCATION_LOST_AND_FOUND:
+        lost_and_found_changed.send(sender=stock, product=stock.product)
+    elif stock.location.type == Location.LOCATION_SUPPLIER:
+        supplier_changed.send(sender=stock, product=stock.product)
+    elif stock.location.type == Location.LOCATION_STORAGE:
+        storage_changed.send(sender=stock, product=stock.product)
+    elif stock.location.type == Location.LOCATION_OUTPUT:
+        output_changed.send(sender=stock, product=stock.product)
+    elif stock.location.type == Location.LOCATION_CUSTOMER:
+        customer_changed.send(sender=stock, product=stock.product)
