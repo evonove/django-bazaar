cd44a09dd2a777e6577d916ca64af3b19e0d772e
Federico Frenguelli <synasius@gmail.com>
2014-10-17 11:10:29 +0200

    fixed stores strategies in tests

--- tests/stores.py
+++ tests/stores.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from bazaar.listings.stores.managers import DefaultStoreManager
+from bazaar.listings.stores.strategies import DefaultStoreStrategy
 
 
-class Store1(DefaultStoreManager):
+class Store1(DefaultStoreStrategy):
     def get_store_name(self):
         return "Store 1"
 
 
-class Store2(DefaultStoreManager):
+class Store2(DefaultStoreStrategy):
     def get_store_name(self):
         return "Store 2"
