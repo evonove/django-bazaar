eabffb064bc2c862448d9f1c295448c10ae3dead
Federico Dolce <psykopear@gmail.com>
2014-10-20 15:41:28 +0200

    Fixed views and urls

--- bazaar/goods/forms.py
+++ bazaar/goods/forms.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
 from django import forms
 from bazaar.goods.models import Product
 from bazaar.helpers import FormHelperMixin
