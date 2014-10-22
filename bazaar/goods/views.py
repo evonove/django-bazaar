ce7e92b1421b04ee1229de8e4a6471382fb83bde
Federico Dolce <psykopear@gmail.com>
2014-10-22 12:40:48 +0200

    Is now possible to order list view by purchse price and stock.

--- bazaar/goods/views.py
+++ bazaar/goods/views.py
@@ -1,23 +1,46 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
-from django.core.urlresolvers import reverse_lazy
 
+from django.core.urlresolvers import reverse_lazy
+from django.utils.datastructures import SortedDict
 from django.views import generic
 
 from braces.views import LoginRequiredMixin
-from bazaar.goods.filters import ProductFilter
-from bazaar.goods.forms import ProductForm
-
-from ..mixins import BazaarPrefixMixin, FilterSortableListView
+from .filters import ProductFilter
+from .forms import ProductForm
 from .models import Product
+from ..warehouse.models import Location
+from ..mixins import BazaarPrefixMixin, FilterSortableListView
 
 
 class ProductListView(LoginRequiredMixin, BazaarPrefixMixin, FilterSortableListView):
     model = Product
     paginate_by = 20
-    fields = ['photo', 'name', 'price', 'ean']
     filter_class = ProductFilter
-    sort_fields = ('name', 'price', 'cost', 'stock')
+    sort_fields = ('name', 'price', 'purchase_price', 'stock')
+
+    def get_queryset(self):
+        qs = super(ProductListView, self).get_queryset()
+        location_storage = Location.objects.get_or_create(type=Location.LOCATION_STORAGE)[0]
+        qs = qs.extra(
+            select=SortedDict([
+                ("stock",
+                 "SELECT SUM(quantity) "
+                 "FROM warehouse_stock "
+                 "WHERE warehouse_stock.product_id = goods_product.id "
+                 "AND warehouse_stock.location_id = %s"),
+                ("purchase_price",
+                 "SELECT SUM(unit_price) "
+                 "FROM warehouse_stock "
+                 "WHERE warehouse_stock.product_id = goods_product.id "
+                 "AND warehouse_stock.location_id = %s"),
+            ]),
+            select_params=(
+                location_storage.id,
+                location_storage.id,
+            )
+        )
+        return qs
 
 
 class ProductDetailView(LoginRequiredMixin, BazaarPrefixMixin, generic.DetailView):
