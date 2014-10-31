139bfd2432cdfd79c0a7f536264dffeb60e02c79
Alex <alessandro.veracchi@evonove.it>
2014-10-31 12:25:31 +0100

    Added tests for ProductQuerySet

--- bazaar/goods/querysets.py
+++ bazaar/goods/querysets.py
@@ -8,7 +8,6 @@ from django.utils.datastructures import SortedDict
 
 from django.utils import timezone
 
-
 FORCED_LOWER = -(sys.maxint - 1)
 
 
@@ -28,14 +27,15 @@ class ProductsQuerySet(models.QuerySet):
             )
         )
 
-    def with_stock_price(self, location_storage_id, location_output_id):
+    def with_total_avr_cost(self, location_storage_id, location_output_id):
         return self.extra(
             select=SortedDict([
-                ("stock_price",
-                 "SELECT AVG(unit_price) "
+                ("total_avr_cost",
+                 "SELECT SUM(unit_price*quantity)/SUM(quantity) "
                  "FROM warehouse_stock "
                  "WHERE warehouse_stock.product_id = goods_product.id "
-                 "AND(warehouse_stock.location_id = %s OR warehouse_stock.location_id = %s)"),
+                 "AND(warehouse_stock.location_id = %s OR warehouse_stock.location_id = %s)"
+                 "AND quantity != 0"),
             ]),
             select_params=(
                 location_storage_id,
@@ -43,19 +43,20 @@ class ProductsQuerySet(models.QuerySet):
             )
         )
 
-    def with_cost(self, location_storage_id):
-
+    def with_total_avr_cost_by_locations(self, location_ids):
+        where_clause = ' 1 = 0 '
+        for location_id in location_ids:
+            where_clause += 'OR warehouse_stock.location_id = %s '
         return self.extra(
             select=SortedDict([
-                ("computed_cost",
-                 "SELECT AVG(unit_price) "
+                ("total_avr_cost",
+                 "SELECT SUM(unit_price*quantity)/SUM(quantity) "
                  "FROM warehouse_stock "
                  "WHERE warehouse_stock.product_id = goods_product.id "
-                 "AND warehouse_stock.location_id = %s"),
+                 "AND({})"
+                 "AND quantity != 0".format(where_clause)),
             ]),
-            select_params=(
-                location_storage_id,
-            )
+            select_params=location_ids
         )
 
     def with_stock_quantity(self, location_storage_id, location_output_id):
@@ -88,7 +89,7 @@ class ProductsQuerySet(models.QuerySet):
     def with_last_sold(self, location_customer_id, time_ago, present=timezone.now()):
         return self.extra(
             select=SortedDict([
-                ("sold_last_month",
+                ("last_sold",
                  "SELECT COALESCE(SUM(warehouse_movement.quantity), %s) "
                  "FROM warehouse_movement WHERE "
                  "warehouse_movement.product_id = goods_product.id "
---
+++ tests/goods/test_querysets.py
@@ -0,0 +1,107 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from bazaar.goods.models import Product
+from bazaar.warehouse import api
+from tests import factories as f
+
+import sys
+
+from django.utils import timezone
+
+FORCED_LOWER = -(sys.maxint - 1)
+
+
+class TestBase(TestCase):
+    def setUp(self):
+        self.time_ago = timezone.now()
+        self.user = get_user_model().objects.create_user(username='test', email='test@test.it', password='test')
+        self.lost_and_found = f.LocationFactory(name='lost and found', slug='lost_and_found', type=4)
+        self.storage = f.LocationFactory(name='storage', slug='storage', type=1)
+        self.output = f.LocationFactory(name='output', slug='output', type=2)
+        self.customer = f.LocationFactory(name='customer', slug='customer', type=3)
+        self.product1 = f.ProductFactory(name='product1', price=2, description='cheap!')
+        self.product2 = f.ProductFactory(name='product2', price=20, description='not cheap!')
+        self.product3 = f.ProductFactory(name='product3', price=200, description='really expensive!')
+
+        # Move products to the warehouse
+        api.move(self.lost_and_found, self.storage, self.product1, 1, 3)
+        api.move(self.lost_and_found, self.storage, self.product2, 10, 30)
+        api.move(self.lost_and_found, self.storage, self.product3, 100, 300)
+
+        api.move(self.storage, self.output, self.product1, 1, 5)
+
+        api.move(self.storage, self.output, self.product3, 5, 500)
+        api.move(self.output, self.customer, self.product3, 2, 500)
+
+
+class TestProductQuerySet(TestBase):
+
+    def test_with_availability(self):
+        product = Product.objects.with_availability(self.storage.id).get(pk=self.product1.id)
+        self.assertEqual(product.availability, 0)
+
+        product = Product.objects.with_availability(self.storage.id).get(pk=self.product2.id)
+        self.assertEqual(product.availability, 10)
+
+        product = Product.objects.with_availability(self.storage.id).get(pk=self.product3.id)
+        self.assertEqual(product.availability, 95)
+
+    def test_with_total_avr_cost(self):
+        product = Product.objects.with_total_avr_cost(self.storage.id, self.output.id).get(pk=self.product1.id)
+        self.assertEqual(product.total_avr_cost, 5)
+
+        product = Product.objects.with_total_avr_cost(self.storage.id, self.output.id).get(pk=self.product2.id)
+        self.assertEqual(product.total_avr_cost, 30)
+
+        product = Product.objects.with_total_avr_cost(self.storage.id, self.output.id).get(pk=self.product3.id)
+        self.assertEqual(product.total_avr_cost, 306)
+
+    def test_with_total_avr_cost_by_locations(self):
+        product = Product.objects.with_total_avr_cost_by_locations((self.storage.id, self.output.id))\
+            .get(pk=self.product1.id)
+        self.assertEqual(product.total_avr_cost, 5)
+
+        product = Product.objects.with_total_avr_cost_by_locations((self.storage.id, self.output.id))\
+            .get(pk=self.product2.id)
+        self.assertEqual(product.total_avr_cost, 30)
+
+        product = Product.objects.with_total_avr_cost_by_locations((self.storage.id, self.output.id))\
+            .get(pk=self.product3.id)
+        self.assertEqual(product.total_avr_cost, 306)
+
+    def test_with_stock_quantity(self):
+        product = Product.objects.with_stock_quantity(self.storage.id, self.output.id).get(pk=self.product1.id)
+        self.assertEqual(product.stock_quantity, 1)
+
+        product = Product.objects.with_stock_quantity(self.storage.id, self.output.id).get(pk=self.product2.id)
+        self.assertEqual(product.stock_quantity, 10)
+
+        product = Product.objects.with_stock_quantity(self.storage.id, self.output.id).get(pk=self.product3.id)
+        self.assertEqual(product.stock_quantity, 98)
+
+    def test_with_sold(self):
+        product = Product.objects.with_sold(self.customer.id).get(pk=self.product1.id)
+        self.assertEqual(product.sold, FORCED_LOWER)
+
+        product = Product.objects.with_sold(self.customer.id).get(pk=self.product2.id)
+        self.assertEqual(product.sold, FORCED_LOWER)
+
+        product = Product.objects.with_sold(self.customer.id).get(pk=self.product3.id)
+        self.assertEqual(product.sold, 2)
+
+    def test_with_last_sold(self):
+        present = timezone.now()
+        product = Product.objects.with_last_sold(self.customer.id, self.time_ago, present=present)\
+            .get(pk=self.product1.id)
+        self.assertEqual(product.last_sold, FORCED_LOWER)
+
+        product = Product.objects.with_last_sold(self.customer.id, self.time_ago, present=present)\
+            .get(pk=self.product2.id)
+        self.assertEqual(product.last_sold, FORCED_LOWER)
+
+        product = Product.objects.with_last_sold(self.customer.id, self.time_ago, present=present)\
+            .get(pk=self.product3.id)
+        self.assertEqual(product.last_sold, 2)
