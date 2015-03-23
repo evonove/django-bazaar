d58aaf2d3b11cfe68899f83c40e37cccc1183625
Federico Dolce <psykopear@gmail.com>
2015-03-23 15:52:19 +0100

    Removed price list and productprice models

--- bazaar/goods/models.py
+++ bazaar/goods/models.py
@@ -14,14 +14,6 @@ from ..fields import MoneyField
 
 
 @python_2_unicode_compatible
-class PriceList(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-@python_2_unicode_compatible
 class Product(models.Model, MoveableProductMixin):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
@@ -29,9 +21,6 @@ class Product(models.Model, MoveableProductMixin):
     code = models.CharField(max_length=20, db_index=True, blank=True)
     photo = models.ImageField(upload_to='products', null=True, blank=True)
     price = MoneyField(help_text=_("Base default price for product"), validators=[MinValueValidator(limit_value=0)])
-
-    # DELETE in future
-    price_lists = models.ManyToManyField("PriceList", through="ProductPrice", related_name="products")
 
     product_type = models.IntegerField(choices=bazaar_settings.PRODUCT_TYPE_CHOICES, null=True, blank=True)
 
@@ -66,16 +55,3 @@ class ProductSet(models.Model):
     class Meta:
         unique_together = ('composite', 'product')
 
-
-@python_2_unicode_compatible
-class ProductPrice(models.Model):
-    price = MoneyField()
-
-    product = models.ForeignKey(Product, related_name="prices")
-    price_list = models.ForeignKey(PriceList, related_name="prices")
-
-    class Meta:
-        unique_together = ('product', 'price_list')
-
-    def __str__(self):
-        return "'%s' %s %s" % (self.product, self.price_list, self.price)
