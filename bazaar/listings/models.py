436860e7bedea3172f288e4cd29f1299999606d3
Federico Dolce <psykopear@gmail.com>
2015-04-01 10:22:59 +0200

    Added inheritance manager to order objects

--- bazaar/listings/models.py
+++ bazaar/listings/models.py
@@ -7,6 +7,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from model_utils.managers import InheritanceManager
 
 from bazaar.listings.managers import PublishingsManager
 from bazaar.listings.querysets import PublishingsQuerySet
@@ -213,6 +214,8 @@ class Order(models.Model):
     quantity = models.IntegerField(default=1)
 
     status = models.IntegerField(max_length=50, choices=ORDER_STATUS_CHOICES, default=ORDER_PENDING)
+
+    objects = InheritanceManager()
 
     def __str__(self):
         return "Order %s from %s" % (self.external_id, self.store)
