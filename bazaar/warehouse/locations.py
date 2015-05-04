2532c7de97935a7460547046d5993a1ebd4ef8f0
Federico Dolce <psykopear@gmail.com>
2015-05-04 18:06:19 +0200

    Fixed get_location

--- bazaar/warehouse/locations.py
+++ bazaar/warehouse/locations.py
@@ -13,30 +13,30 @@ def get_location(slug):
 
 
 def get_storage():
-    return Location.objects.get_or_create(name=bazaar_settings.STORAGE_NAME,
-                                          slug=bazaar_settings.STORAGE,
-                                          type=Location.LOCATION_STORAGE)[0]
+    return Location.objects.get_or_create(slug=bazaar_settings.STORAGE,
+                                          type=Location.LOCATION_STORAGE,
+                                          defaults={'name': bazaar_settings.STORAGE_NAME})[0]
 
 
 def get_output():
-    return Location.objects.get_or_create(name=bazaar_settings.OUTPUT_NAME,
-                                          slug=bazaar_settings.OUTPUT,
-                                          type=Location.LOCATION_OUTPUT)[0]
+    return Location.objects.get_or_create(slug=bazaar_settings.OUTPUT,
+                                          type=Location.LOCATION_OUTPUT,
+                                          defaults={'name': bazaar_settings.OUTPUT_NAME})[0]
 
 
 def get_lost_and_found():
-    return Location.objects.get_or_create(name=bazaar_settings.LOST_AND_FOUND_NAME,
-                                          slug=bazaar_settings.LOST_AND_FOUND,
-                                          type=Location.LOCATION_LOST_AND_FOUND)[0]
+    return Location.objects.get_or_create(slug=bazaar_settings.LOST_AND_FOUND,
+                                          type=Location.LOCATION_LOST_AND_FOUND,
+                                          defaults={'name': bazaar_settings.LOST_AND_FOUND_NAME})[0]
 
 
 def get_customer():
-    return Location.objects.get_or_create(name=bazaar_settings.CUSTOMER_NAME,
-                                          slug=bazaar_settings.CUSTOMER,
-                                          type=Location.LOCATION_CUSTOMER)[0]
+    return Location.objects.get_or_create(slug=bazaar_settings.CUSTOMER,
+                                          type=Location.LOCATION_CUSTOMER,
+                                          defaults={'name': bazaar_settings.CUSTOMER_NAME})[0]
 
 
 def get_supplier():
-    return Location.objects.get_or_create(name=bazaar_settings.SUPPLIER_NAME,
-                                          slug=bazaar_settings.SUPPLIER,
-                                          type=Location.LOCATION_SUPPLIER)[0]
+    return Location.objects.get_or_create(slug=bazaar_settings.SUPPLIER,
+                                          type=Location.LOCATION_SUPPLIER,
+                                          defaults={'name': bazaar_settings.SUPPLIER_NAME})[0]
